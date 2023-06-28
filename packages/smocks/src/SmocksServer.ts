a10ca014e8b6fbb21125171d07016e2a7d989aab
Vlad Churakov <vlchrkv@gmail.com>
2023-06-29 11:19:34 +1200

    feature: improve logging

--- packages/smocks/src/SmocksServer.ts
+++ packages/smocks/src/SmocksServer.ts
@@ -2,11 +2,18 @@ import { Request } from 'express-serve-static-core';
 import express, { Router } from 'express';
 import BodyParser from 'body-parser';
 import { getCurrentInvoke } from '@vendia/serverless-express';
-import { promises as fs } from 'fs';
+import { promises as fs } from 'node:fs';
+import DefaultGateway from 'default-gateway';
+import IpAddr from 'ipaddr.js';
+import Address from 'ipaddr.js';
+import os from 'node:os';
+import Console from 'node:console';
+
 import { RawCollection, RouteConfig } from './types';
 import InMemoryCollectionMapper from './InMemoryCollectionMapper';
 import http from 'http';
 import * as tsImport from 'ts-import';
+import { AddressInfo } from 'net';
 
 const toConvenientRoutes = ({ id, from, routes }: RawCollection): Omit<RawCollection, 'routes'> & { routes: Record<string, string> } => ({
   id,
@@ -16,6 +23,7 @@ const toConvenientRoutes = ({ id, from, routes }: RawCollection): Omit<RawCollec
 
 type Options = {
   port: number;
+  type: 'http' | 'https';
 
   getMockSessionId: (request: Request) => Promise<string | undefined | void>;
 
@@ -44,6 +52,7 @@ class SmocksServer {
       collectionMapper: new InMemoryCollectionMapper(),
       routesDir: 'routes',
       port: 3000,
+      type: 'http',
       ...options,
     };
     this.app.use(BodyParser.urlencoded({ extended: true }));
@@ -51,7 +60,28 @@ class SmocksServer {
       res.on('finish', () => {
         // @ts-ignore
         const { event = {}, context = {} } = getCurrentInvoke();
-        this.log(null, context.awsRequestId, `"${req.method.toUpperCase()} ${decodeURI(req.url)}" ${res.statusCode} ${res.statusMessage}`);
+        const sessionId = (req as any).mockSessionId;
+        const collectionName = (req as any).mockCollectionName;
+        const routeName = (req as any).mockRouteName;
+        this.printServerMessage(
+          null,
+          context.awsRequestId,
+          [
+            `"${req.method.toUpperCase()} ${decodeURI(req.url)}" ${res.statusCode} ${res.statusMessage}`,
+            '[' +
+              [
+                sessionId ? `session=${sessionId}` : '',
+                collectionName ? `collection=${collectionName}` : '',
+                routeName ? `route=${routeName}` : '',
+                `type=${(req as any).mockType}`,
+              ]
+                .filter(Boolean)
+                .join(' ') +
+              ']',
+          ]
+            .filter(Boolean)
+            .join(' ')
+        );
       });
       next();
     });
@@ -60,8 +90,9 @@ class SmocksServer {
       const { event = {}, context = {} } = getCurrentInvoke();
       const router = express.Router() as Router;
 
-      const sessionId = await this.opts.getMockSessionId(req);
+      (req as any).mockSessionId = await this.opts.getMockSessionId(req);
       const collectionName = await this.getCollectionNameForSession(req);
+      (req as any).mockCollectionName = collectionName;
       const collections = await this.loadCollections();
       const routes = await this.loadRoutes();
 
@@ -69,12 +100,12 @@ class SmocksServer {
         throw new Error(`No collection found for "${collectionName}"`);
       }
 
-      this.log(null, context.awsRequestId, `For sessionId="${sessionId}" current collection="${collectionName}"`);
       Object.entries(collections[collectionName]).forEach(([name, variantName]) => {
         routes
           .filter(({ id, variants }) => id === name && variants.find(({ id }) => id === variantName))
           .forEach((route) => {
             router[route.method.toLowerCase() as 'get' | 'post' | 'put'](route.url, async (req, res) => {
+              (req as any).mockRouteName = `${name}:${variantName}`;
               // @ts-ignore
               const { event = {}, context = {} } = getCurrentInvoke();
 
@@ -91,11 +122,11 @@ class SmocksServer {
                 route.body;
               }
               if (variant.type === 'middleware') {
-                this.log(name, context.awsRequestId, `${route.method} "${route.url}" -> Delegating to middleware...`);
+                (req as any).mockType = 'middleware';
                 await variant.options.middleware(req, res, () => {});
                 return;
               }
-              this.log(name, context.awsRequestId, `${route.method} "${route.url}" -> Sending a static response...`);
+              (req as any).mockType = 'static';
               res.statusCode = variant.options.status;
               res.setHeader('Content-Type', 'application/json;charset=UTF-8');
               res.send(JSON.stringify(variant.options.body));
@@ -108,14 +139,15 @@ class SmocksServer {
     this.adminApp.use(express.json());
     this.adminApp.use((req, res, next) => {
       res.on('finish', () => {
-        this.log('ADMIN', null, `"${req.method.toUpperCase()} ${decodeURI(req.url)}" ${res.statusCode} ${res.statusMessage}`);
+        const adminAction = (req as any).mockAdminAction;
+        this.printServerMessage('ADMIN', null, `"${req.method.toUpperCase()} ${decodeURI(req.url)}" ${res.statusCode} ${res.statusMessage} -> ${adminAction}`);
       });
       next();
     });
     this.adminApp.put('/session/:sessionId', (req, res, _next) => {
       const { collection } = req.body;
       const { sessionId } = req.params;
-      this.log('ADMIN', null, `Setting sessionId="${sessionId}" to use collection="${collection}"`);
+      (req as any).mockAdminAction = `Set sessionId="${sessionId}" to use collection="${collection}"`;
       this.opts.collectionMapper.setCollectionName(sessionId, collection).then(() => {
         res.statusCode = 200;
         res.send();
@@ -127,20 +159,19 @@ class SmocksServer {
     if (this.server) {
       throw new Error('Smocks is already running');
     }
-    return Promise.all([
+    await Promise.all([
       new Promise<void>((res) => {
-        this.server = this.app.listen(this.opts.port, () => {
-          console.log(`Smocks is listening to ${this.opts.port}`);
-          res();
-        });
+        this.server = this.app.listen(this.opts.port, res);
       }),
       new Promise<void>((res) => {
-        this.adminServer = this.adminApp.listen(3001, () => {
-          console.log(`Smocks ADMIN is listening to 3001`);
-          res();
-        });
+        this.adminServer = this.adminApp.listen(3001, res);
       }),
     ]);
+
+    this.print('info', `Smocks is running at:`);
+    await this.dumpSeverUrls(this.server!, this.opts.type);
+    this.print('info', `Smocks ADMIN is running at:`);
+    await this.dumpSeverUrls(this.adminServer!, this.opts.type);
   }
   async stop() {
     if (!this.server) {
@@ -153,7 +184,7 @@ class SmocksServer {
         }
         this.server.close((err) => {
           if (err) {
-            console.error(err);
+            this.print('error', err);
             return;
           }
           this.server = undefined;
@@ -166,7 +197,7 @@ class SmocksServer {
         }
         this.adminServer.close((err) => {
           if (err) {
-            console.error(err);
+            this.print('error', err);
             return;
           }
           this.adminServer = undefined;
@@ -174,11 +205,11 @@ class SmocksServer {
         });
       }),
     ]);
-    console.log('Smocks stopped');
+    this.print('info', 'Smocks stopped');
   }
 
-  private log(name: string | null, reqId: string | null, message: string) {
-    console.log([`[${new Date().toISOString()}]`, reqId ? `[${reqId}]` : null, name ? `[${name}]` : null, message].filter(Boolean).join(' '));
+  private printServerMessage(name: string | null, reqId: string | null, message: string) {
+    this.print('info', [`[${new Date().toISOString()}]`, reqId ? `[${reqId}]` : null, name ? `[${name}]` : null, message].filter(Boolean).join(' '));
   }
 
   private async getCollectionNameForSession(req: Request): Promise<string> {
@@ -217,6 +248,80 @@ class SmocksServer {
     return (await Promise.all(files.map((filename) => tsImport.load(this.opts.routesDir + '/' + filename, { compiledJsExtension: 'cjs' }))))
       .map((m) => m.default)
       .flat();
+  }
+
+  private async dumpSeverUrls(server: http.Server, protocol: 'http' | 'https') {
+    const { address, port } = server.address() as AddressInfo;
+    const parsedIP = IpAddr.parse(address);
+    const prettyPrintURL = (hostname: string) => {
+      let url = `${protocol}://${hostname}`;
+      if (port) {
+        url += `:${port}`;
+      }
+      return url + '/';
+    };
+
+    if (parsedIP.range() === 'unspecified') {
+      this.print('info', `\tLoopback: ${prettyPrintURL('localhost')}`);
+
+      const networkIPv4 = await this.internalIP('v4');
+      if (networkIPv4) {
+        this.print('info', `\tOn Your Network (IPv4): ${prettyPrintURL(networkIPv4)}`);
+      }
+      const networkIPv6 = await this.internalIP('v6');
+      if (networkIPv6) {
+        this.print('info', `\tOn Your Network (IPv6): ${prettyPrintURL(networkIPv6)}`);
+      }
+    } else if (parsedIP.range() === 'loopback') {
+      if (parsedIP.kind() === 'ipv4' || parsedIP.kind() === 'ipv6') {
+        this.print('info', `\tLoopback: ${prettyPrintURL(parsedIP.toString())}`);
+      }
+    } else {
+      this.print(
+        'info',
+        `\tOn Your Network (IPv4): ${
+          parsedIP.kind() === 'ipv6' && (parsedIP as Address.IPv6).isIPv4MappedAddress()
+            ? prettyPrintURL((parsedIP as Address.IPv6).toIPv4Address().toString())
+            : prettyPrintURL(address)
+        }`
+      );
+      if (parsedIP.kind() === 'ipv6') {
+        this.print('info', `\tOn Your Network (IPv6): ${prettyPrintURL(address)}`);
+      }
+    }
+  }
+
+  private async internalIP(family: 'v4' | 'v6'): Promise<string | undefined> {
+    try {
+      const { gateway } = await (family === 'v6' ? DefaultGateway.gateway6async() : DefaultGateway.gateway4async());
+      const gatewayIp = IpAddr.parse(gateway);
+      for (const addresses of Object.values(os.networkInterfaces()).filter(Boolean)) {
+        for (const { cidr } of addresses!) {
+          if (!cidr) {
+            continue;
+          }
+          const net = IpAddr.parseCIDR(cidr);
+          if (net[0] && net[0].kind() === gatewayIp.kind() && gatewayIp.match(net)) {
+            return net[0].toString();
+          }
+        }
+      }
+    } catch (e) {}
+    return undefined;
+  }
+
+  private print(level: 'info' | 'warn' | 'error', ...args: Parameters<typeof Console.log>) {
+    switch (level) {
+      case 'error':
+        Console.error(...args);
+        break;
+      case 'warn':
+        Console.warn(...args);
+        break;
+      case 'info':
+        Console.log(...args);
+        break;
+    }
   }
 }
 
