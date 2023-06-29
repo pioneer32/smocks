d9c6162370b90a2eaca4b25cbbf5ef6ceb3c2c15
Vlad Churakov <vlchrkv@gmail.com>
2023-06-29 18:34:00 +1200

    test 3

---
+++ packages/smocks/scripts/patch-package_json.cjs
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+const fs = require('node:fs');
+const path = require('node:path');
+
+const packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'package.json')));
+
+delete packageJson.exports['.'].defaults;
+
+fs.writeFileSync(path.join(__dirname, '..', 'package.json'), JSON.stringify(packageJson));
