37d0741e112d8f0c6650c3eaf659426847a010ec
Vlad Churakov <vlchrkv@gmail.com>
2023-11-29 17:23:43 +1300

    fix: Packaging of esm and cjs

--- packages/smocks/scripts/patch-package_json.cjs
+++ packages/smocks/scripts/patch-package_json.cjs
@@ -6,6 +6,5 @@ const path = require('node:path');
 const packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'package.json')));
 
 delete packageJson.exports['.'].default;
-packageJson.type = 'commonjs';
 
 fs.writeFileSync(path.join(__dirname, '..', 'package.json'), JSON.stringify(packageJson, null, 4));
